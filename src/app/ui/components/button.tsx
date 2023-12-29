fe37b836be05df87498a97802e3feac0b2233a0a
Yunze Shi <yunzeshi@outlook.com>
2023-12-29 22:32:32 +1100

    feat (close #2) wip homepage

--- src/app/ui/components/button.tsx
+++ src/app/ui/components/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils/cn';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  'items-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
@@ -21,10 +21,21 @@ const buttonVariants = cva(
         lg: 'h-10 rounded-md px-8',
         icon: 'h-9 w-9',
       },
+      flow: {
+        inline: 'inline-flex',
+        block: 'flex',
+      },
+      justify: {
+        default: ' justify-center',
+        start: ' justify-start',
+        end: ' justify-end',
+      },
     },
     defaultVariants: {
       variant: 'default',
       size: 'default',
+      flow: 'inline',
+      justify: 'default',
     },
   },
 );
--- src/app/ui/explorer.tsx
+++ src/app/ui/explorer.tsx
@@ -1,18 +1,23 @@
 import Link from 'next/link';
 import { getAllNotes } from '@/app/api/notes';
 import { buttonVariants } from '@/components/button';
+import { Pencil2Icon } from '@radix-ui/react-icons';
 
 export default async function Explorer() {
   const notes = await getAllNotes();
   return (
     <aside className="dark:00 w-80 bg-gray-200 px-4 py-8 text-sm">
-      <Link className={buttonVariants({ variant: 'outline' })} href={''}>
-        Click here
+      <Link className={buttonVariants({ justify: 'start', variant: 'ghost', flow: 'block' })} href={'/'}>
+        <Pencil2Icon className="mr-2" />
+        New note
       </Link>
       <ul className="flex flex-col gap-1">
         {notes.map(({ id, title }) => (
-          <li className="px-4 py-2 capitalize" key={id}>
-            <Link className="block" href={`/notes/${id}`}>
+          <li className="capitalize" key={id}>
+            <Link
+              className={buttonVariants({ justify: 'start', variant: 'ghost', flow: 'block' })}
+              href={`/notes/${id}`}
+            >
               {title}
             </Link>
           </li>
