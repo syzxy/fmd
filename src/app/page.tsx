68b7a2ce259ef7d7d8aefb207895d8501cf200c7
Yunze Shi <yunzeshi@outlook.com>
2023-12-29 13:39:15 +1100

    style: workable dark mode colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { NoteEditor } from './ui/editor/editor';
 export default function Home() {
   return (
     <div className="mx-auto max-w-4xl">
-      <NoteEditor className="prose lg:prose-xl" />
+      <NoteEditor className="prose dark:prose-invert lg:prose-xl" />
     </div>
   );
 }
--- src/app/ui/explorer.tsx
+++ src/app/ui/explorer.tsx
@@ -4,7 +4,7 @@ import { getAllNotes } from '@/app/api/notes';
 export default async function Explorer() {
   const notes = await getAllNotes();
   return (
-    <aside className="w-80 bg-gray-200 px-4 py-8 text-sm">
+    <aside className="w-80 bg-gray-200 dark:bg-slate-900 px-4 py-8 text-sm">
       <ul className="flex flex-col gap-1">
         {notes.map(({ id, title }) => (
           <li className="p-2 capitalize" key={id}>
