fa1a933ded05ad428f2655d0f0d9b38d08a300a1
Yunze Shi <yunzeshi@outlook.com>
2023-12-29 15:45:15 +1100

    feat: added shadcn as ui lib

--- src/app/ui/explorer.tsx
+++ src/app/ui/explorer.tsx
@@ -1,13 +1,17 @@
 import Link from 'next/link';
 import { getAllNotes } from '@/app/api/notes';
+import { buttonVariants } from '@/components/button';
 
 export default async function Explorer() {
   const notes = await getAllNotes();
   return (
-    <aside className="w-80 bg-gray-200 dark:bg-slate-900 px-4 py-8 text-sm">
+    <aside className="dark:00 w-80 bg-gray-200 px-4 py-8 text-sm">
+      <Link className={buttonVariants({ variant: 'outline' })} href={''}>
+        Click here
+      </Link>
       <ul className="flex flex-col gap-1">
         {notes.map(({ id, title }) => (
-          <li className="p-2 capitalize" key={id}>
+          <li className="px-4 py-2 capitalize" key={id}>
             <Link className="block" href={`/notes/${id}`}>
               {title}
             </Link>
